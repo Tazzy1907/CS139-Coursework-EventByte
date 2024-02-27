5c05050c027fe93b71f2c226bb8f3a0df079ec02
Taz <u5503732@live.warwick.ac.uk>
2024-02-27 15:33:44 +0000

    Events now drop down on the my event page. Need to make it obvious they can be clicked.

---
+++ coursework/static/js/boughtEventsView.js
@@ -0,0 +1,11 @@
+$('#upcoming').click(function() {
+    $('#upcomingEvents').slideToggle("slow");
+})
+
+$('#cancelled').click(function() {
+    $('#cancelledEvents').slideToggle("slow");
+})
+
+$('#completed').click(function() {
+    $('#completedEvents').slideToggle("slow");
+})
